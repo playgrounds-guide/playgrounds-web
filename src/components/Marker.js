b4d6c31dff34fa3a2883f738d9228373e8bd222f
Dominika Kosiedowska <d.kosiedowska@gmail.com>
2017-08-22 15:37:29 +0200

    tooltip with image content

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Tooltip} from 'react-lightweight-tooltip';
 import marker from '../images/marker.png';
+import test from '../images/playground-test.jpg'
 
-const Marker = () => <Tooltip content="here"><img src={marker} alt='playground' style={{width: 35, height: 35, position: 'absolute', top: -35, left: -35/2}}/></Tooltip>
+const Marker = () => <Tooltip content={<img src={test}/>}><img src={marker} alt='playground' style={{width: 35, height: 35, position: 'absolute', top: -35, left: -35/2}}/></Tooltip>
 
 export default Marker;
